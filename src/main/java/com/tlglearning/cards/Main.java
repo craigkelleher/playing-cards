5599b4b7ebb694706be17434be4bde1267ff757b
craigkelleher <kelleher.craig@gmail.com>
2022-09-27 14:02:02 -0700

    ColorFirst is a local class

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,6 +9,23 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
+
+    class ColorFirst implements Comparator<Card> {
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0){
+          comparison = card1.suit().compareTo(card2.suit());
+          if (comparison == 0){
+            //computer all on right, and then turn into negative number, assigning to comparison.
+            comparison = - card1.rank().compareTo(card2.rank()); //- reverses comparison
+          }
+        }
+        return comparison;
+      }
+    }
+
+
     // Create instance of deck
     Deck deck = new Deck();
     // Print the string representation of the deck
@@ -26,18 +43,4 @@ public class Main {
     System.out.println(deck);
   }
 
-  private static class ColorFirst implements Comparator<Card> {
-    @Override
-    public int compare(Card card1, Card card2) {
-      int comparison = card1.suit().color().compareTo(card2.suit().color());
-      if (comparison == 0){
-        comparison = card1.suit().compareTo(card2.suit());
-        if (comparison == 0){
-          //computer all on right, and then turn into negative number, assigning to comparison.
-          comparison = - card1.rank().compareTo(card2.rank()); //- reverses comparison
-        }
-      }
-      return comparison;
-    }
-  }
 }
