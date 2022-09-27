9778c17859f98f6bb1922ba28e81816362733970
craigkelleher <kelleher.craig@gmail.com>
2022-09-27 14:20:33 -0700

    in-line Anonymous class in main method

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,23 +9,6 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
-
-    class ColorFirst implements Comparator<Card> {
-      @Override
-      public int compare(Card card1, Card card2) {
-        int comparison = card1.suit().color().compareTo(card2.suit().color());
-        if (comparison == 0){
-          comparison = card1.suit().compareTo(card2.suit());
-          if (comparison == 0){
-            //computer all on right, and then turn into negative number, assigning to comparison.
-            comparison = - card1.rank().compareTo(card2.rank()); //- reverses comparison
-          }
-        }
-        return comparison;
-      }
-    }
-
-
     // Create instance of deck
     Deck deck = new Deck();
     // Print the string representation of the deck
@@ -39,7 +22,21 @@ public class Main {
     System.out.println(deck);
     deck.sort();
     System.out.println(deck);
-    deck.sort(new ColorFirst()); //create an instance
+    deck.sort(new Comparator<>() {
+
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0) {
+          comparison = card1.suit().compareTo(card2.suit());
+          if (comparison == 0) {
+            //computer all on right, and then turn into negative number, assigning to comparison.
+            comparison = - card1.rank().compareTo(card2.rank());
+          }
+        }
+        return comparison;
+      }
+    }); //create an instance
     System.out.println(deck);
   }
 
